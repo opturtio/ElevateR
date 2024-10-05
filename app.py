859e6296499eb4bf5895cd7832cde28b143cab9b
opturtio <olli-pekka.turtio@helsinki.fi>
2024-10-05 20:35:37 +0300

    CSS file works and added keyframe and hover on elevate button

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from backend.fetch_stations import fetch_helsinki_stations
 
-app = Flask(__name__, template_folder="frontend/templates")
+app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
 app.secret_key = getenv("SECRET_KEY")
 CORS(app)
 
